97380ca4e372ea9a824e55621d30649250fbeb5f
root <root@ip-172-31-18-134.ec2.internal>
2025-07-03 09:38:50 +0000

    xnnc

--- numbers.java
+++ numbers.java
@@ -2,7 +2,23 @@ Public class login
 {
    public static void main()
    {
-       int i =0;
+       int i =0   public static void main()
+{  {
+       int i =0;
+
+
+
+       for (i=0;i<==20;i++
+
+
+
+
+
+       {
+            System.out Println ("Number: "+i);
+       }
+   }
+};
        
        for (i=0;i<==30;i++     
    
